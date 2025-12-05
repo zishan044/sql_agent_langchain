0002485b2a76aa0ab1f1fb57d17cfd61c93c4916
zishan044 <winchesterfelix007@gmail.com>
2025-12-05 21:02:43 +0600

    langchain agent implemented

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests, pathlib
 from langchain_ollama import ChatOllama
 from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain.agents import create_agent
 
 def main():
-    model = ChatOllama(
-        model="llama3.1:8b",
+    llm = ChatOllama(
+        model="llama3.1:latest",
         temperature=0.7,
     )
 
@@ -23,11 +25,51 @@ def main():
     
     db = SQLDatabase.from_uri('sqlite:///Chinook.db')
 
-    print(f"Dialect: {db.dialect}")
-    print(f"Available tables: {db.get_usable_table_names()}")
-    print(f'Sample output: {db.run("SELECT * FROM Artist LIMIT 5;")}')
+    # print(f"Dialect: {db.dialect}")
+    # print(f"Available tables: {db.get_usable_table_names()}")
+    # print(f'Sample output: {db.run("SELECT * FROM Artist LIMIT 5;")}')
 
+    toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+    tools = toolkit.get_tools()
+    # for tool in tools:
+    #     print(f"{tool.name}: {tool.description}\n")
 
+    
+    system_prompt = """
+    You are an agent designed to interact with a SQL database.
+    Given an input question, create a syntactically correct {dialect} query to run,
+    then look at the results of the query and return the answer. Unless the user
+    specifies a specific number of examples they wish to obtain, always limit your
+    query to at most {top_k} results.
+
+    You can order the results by a relevant column to return the most interesting
+    examples in the database. Never query for all the columns from a specific table,
+    only ask for the relevant columns given the question.
+
+    You MUST double check your query before executing it. If you get an error while
+    executing a query, rewrite the query and try again.
+
+    DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the
+    database.
+
+    To start you should ALWAYS look at the tables in the database to see what you
+    can query. Do NOT skip this step.
+
+    Then you should query the schema of the most relevant tables.
+    """.format(
+        dialect=db.dialect,
+        top_k=5,
+    )
+
+    agent = create_agent(model=llm, tools=tools, system_prompt=system_prompt)
+
+    question = "Which genre on average has the longest tracks?"
+
+    for step in agent.stream(
+        {"messages": [{"role": "user", "content": question}]},
+        stream_mode="values",
+    ):
+        step["messages"][-1].pretty_print()
 
 if __name__ == "__main__":
     main()
